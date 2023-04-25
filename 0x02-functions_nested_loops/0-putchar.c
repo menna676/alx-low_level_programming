86dda0a2a592909c93e84826f9e96e0ed3e39748
menna676 <Mennahany.dice@gmail.com>
2023-04-25 07:47:25 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* main - Entry point
-* Description: 'prints _putchar to stdout'
-* Return always 0
+*main - Entry point
+*Description: 'prints _putchar to stdout'
+*Return: always 0
 */
 int main(void)
 {
