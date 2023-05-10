ba4d074911ed8d2d80963bbf815115ed699b461f
menna676 <Mennahany.dice@gmail.com>
2023-05-10 14:45:54 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,7 +14,7 @@ return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
 * _strlen_recursion - returns the length of a string
-* @s: string to calculate the length
+* @s: string to calculate the length of
 * Return: length of the string
 */
 int _strlen_recursion(char *s)
