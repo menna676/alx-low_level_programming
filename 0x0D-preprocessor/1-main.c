bddabbe98eb4295c0b8be6159968760e10740bd4
menna676 <Mennahany.dice@gmail.com>
2023-05-23 10:33:06 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,16 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+/**
+* main - check the codr
+* Return: always 0
+*/
+int main(void)
+{
+float a;
+float r;
+r = 98;
+a = PI * r *r;
+printf("%.3f\n", a);
+return (0);
+}
