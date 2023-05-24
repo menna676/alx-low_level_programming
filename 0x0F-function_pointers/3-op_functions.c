fa25e7782e94b8c8dc5b955943afef222c21b4a2
menna676 <Mennahany.dice@gmail.com>
2023-05-24 13:04:28 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,7 @@ int op_mod(int a, int b);
 * op_add - returns the sum of two numbers
 * @a: the first number
 * @b: the second number
-* Return: the sum of two numbers
+* Return: the sum of a and b
 */
 int op_add(int a, int b)
 {
@@ -18,7 +18,7 @@ return (a + b);
 * op_sub - returns the difference of two numbers
 * @a: the first number
 * @b: the second number
-* Return: the diffrence of two numbers
+* Return: the diffrence of a and b
 */
 int op_sub(int a, int b)
 {
@@ -28,7 +28,7 @@ return (a - b);
 * op_mul - returns the product of two numbers
 * @a: the first number
 * @b: the second number
-* Return: the product of two numbers
+* Return: the product of a and b
 */
 int op_mul(int a, int b)
 {
@@ -38,7 +38,7 @@ return (a * b);
 * op_div - returns the division of two numbers
 * @a: the first number
 * @b: the second number
-* Return: the qoutient of two numbers
+* Return: the qoutient of a and b
 */
 int op_div(int a, int b)
 {
@@ -48,7 +48,7 @@ return (a / b);
 * op_mod - returns the remainder of the division of two numbers
 * @a: the first number
 * @b: the second number
-* Return: the remainder of the division of two numbers
+* Return: the remainder of the division of a and b
 */
 int op_mod(int a, int b)
 {
